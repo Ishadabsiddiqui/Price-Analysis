21967b19857650acec7a35d1363eddc6dcfc34a0
Shadab Siddiqui <shadabsiddiqui547@gmail.com>
2023-12-21 02:52:03 +0530

    m

--- next.config.js
+++ next.config.js
@@ -2,13 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-    serverComponentsExternalPackages: ['@acme/ui']
-  },
-  images: {
-    domains: ['m.media-amazon.com']
-  }
+
 }
 
 module.exports = nextConfig
